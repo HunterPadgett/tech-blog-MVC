a8b26a585386311a5d9f4020755005edebf4165b
Hunter Padgett <gizmokabanjo@gmail.com>
2022-07-27 23:56:06 -0400

    worked on models

---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        isAlphanumeric: true,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'blog',
+        key: 'id'
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,10 +1,30 @@
-const X = require('./X')
-const Y = require('./Y')
-const Z = require('./Z')
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
+});
 
 
-
-
-
-
-module.exports = { X, Y, Z };
+module.exports = { User, Blog, Comment };
