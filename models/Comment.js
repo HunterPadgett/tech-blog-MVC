86795d78799c4da539bcc2f7c0bdd959f0fba452
Hunter Padgett <gizmokabanjo@gmail.com>
2022-07-28 22:52:40 -0400

    added some handlebar pages

--- models/Comment.js
+++ models/Comment.js
@@ -1,48 +1,48 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Comment extends Model {}
+// class Comment extends Model {}
 
-Comment.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true
-    },
-    comment: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-        isAlphanumeric: true,
-      },
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'user',
-        key: 'id'
-      }
-    },
-    blog_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'blog',
-        key: 'id'
-      }
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment',
-  }
-);
+// Comment.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       primaryKey: true,
+//       allowNull: false,
+//       autoIncrement: true
+//     },
+//     comment: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         len: [1],
+//         isAlphanumeric: true,
+//       },
+//     },
+//     user_id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: 'user',
+//         key: 'id'
+//       }
+//     },
+//     blog_id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: 'blog',
+//         key: 'id'
+//       }
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'comment',
+//   }
+// );
 
-module.exports = Comment;
+// module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Blog = require('./Blog');
-const Comment = require('./Comment');
+// const Comment = require('./Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
@@ -10,21 +10,21 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-});
+// User.hasMany(Comment, {
+//   foreignKey: 'user_id',
+// });
 
-Comment.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Comment.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
-Blog.hasMany(Comment, {
-  foreignKey: 'blog_id',
-});
+// Blog.hasMany(Comment, {
+//   foreignKey: 'blog_id',
+// });
 
-Comment.belongsTo(Blog, {
-  foreignKey: 'blog_id'
-});
+// Comment.belongsTo(Blog, {
+//   foreignKey: 'blog_id'
+// });
 
 
-module.exports = { User, Blog, Comment };
+module.exports = { User, Blog };
